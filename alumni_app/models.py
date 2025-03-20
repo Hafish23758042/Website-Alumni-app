90d72b3d03d4109df9c3465df70bf99f55f974fe
Hafish23758042 <nagaapi1145@gmail.com>
2025-03-20 23:37:35 +0700

    penambahan style website dan UI/UX

--- alumni_app/models.py
+++ alumni_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Alumni(models.Model):
- name = models.CharField(max_length=100)
- email = models.EmailField(unique=True)
- graduation_year = models.IntegerField()
- major = models.CharField(max_length=100)
- job_position = models.CharField(max_length=100)
- company = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    graduation_year = models.IntegerField()
+    major = models.CharField(max_length=100)
+    job_position = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
